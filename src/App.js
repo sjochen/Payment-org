59f2cd39d58bc9491f053a91f51973216f2eb17d
Stephen Jochen <stephenjochen2@gmail.com>
2019-11-03 23:16:50 -0500

    got the pagination working and finished styling

--- src/App.js
+++ src/App.js
@@ -3,33 +3,66 @@ import PaymentCard from "./components/PaymentCard";
 import Wrapper from "./components/Wrapper";
 import Title from "./components/Title";
 import pay from "./pay.json";
-
+import logo from "./components/Images/paymerang.png"
 class App extends Component {
-  constructor(props) {
-    super(props);
+
+  constructor() {
+    super();
     this.state = {
-      activePage: 1,
-      pay
+      pay,
+      currentPage: 1,
+      todosPerPage: 3
     };
-  }
-  handlePageChange(pageNumber) {
-    console.log(`active page is ${pageNumber}`);
-    this.setState({activePage: pageNumber});
+    this.handleClick = this.handleClick.bind(this);
   }
 
+  handleClick(event) {
+    this.setState({
+      currentPage: Number(event.target.id)
+    });
+  }
 
 
   render() {
+    const { pay, currentPage, todosPerPage } = this.state;
+
+    // Logic for displaying todos
+    const indexOfLastTodo = currentPage * todosPerPage;
+    const indexOfFirstTodo = indexOfLastTodo - todosPerPage;
+    const currentTodos = pay.slice(indexOfFirstTodo, indexOfLastTodo);
+
+    // Logic for displaying page numbers
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(pay.length / todosPerPage); i++) {
+      pageNumbers.push(i);
+    }
+
+    const renderPageNumbers = pageNumbers.map(number => {
+      return (
+        <span tabIndex="1"
+          key={number}
+          id={number}
+          onClick={this.handleClick}
+        >
+          {number}
+        </span>
+      );
+    });
     return (
       <Wrapper>
-        <Title>List Of Payees</Title>
-        {this.state.pay.map(pay => (
+        <Title><img className="payImg" src={logo} alt="Logo" /></Title>
+        {currentTodos.map(pay => (
           <PaymentCard
             Payee={pay.Payee}
             Payment={pay.Payment}
             Remittance={pay.Remittance}
           />
-        ))};
+        ))}
+        <div>
+          <ul className="pagination">
+            {renderPageNumbers}
+          </ul>
+        </div>
       </Wrapper>
     );
   }
--- src/components/PaymentCard/index.js
+++ src/components/PaymentCard/index.js
@@ -5,47 +5,31 @@ function PaymentCard(props) {
   return (
     <div className="card">
       <div className="content">
-        <ul>
-          <li>
-            <h3><strong>Payee:</strong></h3>
-            <p><strong>Name:</strong> {props.Payee.Name}</p>
-            <p><strong>Fax:</strong> {props.Payee.Fax}</p>
-            <p><strong>Phone:</strong> {props.Payee.Phone}</p>
-            <p><strong>Address:</strong> {props.Payee.Address.Address1}</p>
-            <p><strong>City:</strong> {props.Payee.Address.City}</p>
-            <p><strong>State:</strong> {props.Payee.Address.StateOrProvince}</p>
-            <p><strong>Country:</strong> {props.Payee.Address.Country}</p>
-            <p><strong>Postal Code:</strong> {props.Payee.Address.PostalCode}</p>
-            <p><strong>Attention:</strong> {props.Payee.Attention}</p>
-            <p><strong>Submission Date:</strong> {props.Payee.SubmissionDate}</p>
+        <h3><strong>Payee:</strong></h3>
+        <p><strong>Name:</strong> {props.Payee.Name}</p>
+        <p><strong>Fax:</strong> {props.Payee.Fax}  <strong>Phone:</strong> {props.Payee.Phone}</p>
+        <p><strong>Address:</strong> {props.Payee.Address.Address1}</p>
+        <p><strong>City/State:</strong> {props.Payee.Address.City}, {props.Payee.Address.StateOrProvince} <strong>Postal Code:</strong> {props.Payee.Address.PostalCode}</p>
+        <p><strong>Country:</strong> {props.Payee.Address.Country}</p>
+        <p><strong>Attention:</strong> {props.Payee.Attention}</p>
+        <p><strong>Submission Date:</strong> {props.Payee.SubmissionDate}</p>
+        <h3><strong>Payment:</strong></h3>
+        <p className="inv"><strong>PAN:</strong> {props.Payment.PAN}</p>
+        <p className="inv"><strong>CVV:</strong> {props.Payment.CVV}</p>
+        <p className="inv"><strong>Exp:</strong> {props.Payment.Exp}</p>
 
-
-          </li>
-          <li>
-            <h3><strong>Payment:</strong></h3>
-            <p><strong>PAN:</strong> {props.Payment.PAN}</p>
-            <p><strong>CVV:</strong> {props.Payment.CVV}</p>
-            <p><strong>Exp:</strong> {props.Payment.Exp}</p>
-          </li>
-          <li>
-            <h3><strong>Remittance:</strong></h3>
-            {props.Remittance.map((name, index) => {
-              return (
-                <div>
-                  <p><strong>Payor Name:</strong> {name.PayorName}</p>
-                  <p><strong>Payor ID:</strong> {name.PayorId}</p>
-                  <p><strong>Invoice No.:</strong> {name.InvoiceNo}</p>
-                  <p><strong>Description:</strong> {name.Description}</p>
-                  <p><strong>Amount:</strong> {name.Amount}</p>
-                </div>
-              )
-            })}
-          </li>
-        </ul>
+        <h3><strong>Remittance:</strong></h3>
+        {props.Remittance.map((name, index) => {
+          return (
+            <div className="rem">
+              <p className="inv"><strong>Payor Name:</strong> <mark className="highlight">{name.PayorName}</mark></p>
+              <p className="inv"><strong>Invoice No.:</strong> <mark className="highlight">{name.InvoiceNo}</mark> <strong>Payor ID:</strong> <mark className="highlight">{name.PayorId}</mark></p>
+              <p><strong>Description:</strong> {name.Description}</p>
+              <p className="amount"><strong>Amount:</strong> <mark className="highlight">{name.Amount}</mark></p>
+            </div>
+          )
+        })}
       </div>
-      <span onClick={() => props.removeFriend(props.id)} className="remove">
-        𝘅
-      </span>
     </div>
   );
 }
